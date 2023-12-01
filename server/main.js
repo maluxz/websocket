ff35d9fe030a6c2d21c7dc468a618b077173708d
Mario <malux97@hotmail.com>
2023-12-01 09:59:27 -0600

    Primera implementación de socket.io

--- server/main.js
+++ server/main.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-
 var server = require('http').Server(app);
 
 var io = require('socket.io')(server);
@@ -9,6 +8,12 @@ app.get('/',function(request, response) {
     response.status(200).send('Hola mundo');
 });
 
+
+// De esta forma activamos sockt para que este ecuchando mandamos un mensaje de control por consola para saber que pasa y tenemos que hacer ocn el mensaje venga del navegaor web mediante html u JS
+io.on('connection', function(socket) {
+    console.log('Alguien se ha conectado con socket');
+});
+
 server.listen(3002, function() {
     console.log('El servidor esta corriendo en http://localhost:3002');
-})
+});
