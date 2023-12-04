aa841593672243526a6023153fe6791d1b197212
Mario <malux97@hotmail.com>
2023-12-04 09:16:24 -0600

    middleware para estáticos

--- server/main.js
+++ server/main.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-
 var io = require('socket.io')(server);
+
+// Usamos un middleware para usar elementos estáticos en la sección pública de la aplicación
+app.use(express.static('public'));
 
 app.get('/',function(request, response) {
     response.status(200).send('Hola mundo');
 });
-
 
 // De esta forma activamos sockt para que este ecuchando mandamos un mensaje de control por consola para saber que pasa y tenemos que hacer ocn el mensaje venga del navegaor web mediante html u JS
 io.on('connection', function(socket) {
